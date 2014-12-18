cf8e165a14b7e7d01e596751ba021e250a2dcd8f
Arnaud MESUREUR <arnaud.mesureur@gmail.com>
2014-12-18 23:50:09 +0100

    add favicon and active tab directive

--- app/scripts/controllers/home-controller.js
+++ app/scripts/controllers/home-controller.js
@@ -10,5 +10,21 @@ angular.module('problemhunt')
   });
 })
 .controller('HomeController', function($scope, $rootScope, $state, Auth) {
-  
-});
+})
+.directive('activeTab', function($state, $compile) {
+  return {
+    link: function($scope, element, attrs) {
+      $scope.$on("$stateChangeSuccess", function (event, current, previous) {
+        for (var i = 0; i < element[0].children.length; ++i) {
+          var li = element[0].children[i];
+          var a = li.children[0];
+          if (a.attributes['ui-sref'].value === current.name) {
+            $(li).addClass('active');
+          } else {
+            $(li).removeClass('active');
+          }
+        }
+      });
+    }
+  }
+})
